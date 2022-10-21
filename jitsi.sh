5a93b61f667bedc4ef8e39e2733bd7287565d32c
Benjamín Sepúlveda <alonsso008@gmail.com>
2022-10-20 22:25:59 -0300

    aaaa

--- jitsi.sh
+++ jitsi.sh
@@ -20,8 +20,6 @@ systemctl start nginx.service
 
 systemctl enable nginx.service
 
-**Instalamos Jitsi**
-
 curl https://download.jitsi.org/jitsi-key.gpg.key | sudo sh -c 'gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg'
 
 echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | sudo tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
